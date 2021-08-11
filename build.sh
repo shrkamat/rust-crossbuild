481ac2b75b9b6742457e4037a1b547d451a5645f
shrkamat <skamath@synamedia.com>
2021-08-11 13:18:05 +0530

    Lost track on this, just commiting

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 cargo build --release
 
-strip ./target/release/cross
-./target/release/cross
-du -sh ./target/release/cross
+# not suure why cargo strip is not working for cross compilers
+cargo strip
+
+/opt/BLD_SAM_BCM_MIPS4K_LNUX_ASTRO_06/platform_cfg/linux/compiler/brcm_gcc_x86_linux_06/bin/aarch64-linux-gnu-strip ./target/aarch64-unknown-linux-gnu/release/cross
+file ./target/aarch64-unknown-linux-gnu/release/cross
+readelf -d ./target/aarch64-unknown-linux-gnu/release/cross
+du -sh ./target/aarch64-unknown-linux-gnu/release/cross
